9fee7466ed1fe7611315db94dda9464ed536f0bc
Adam Purcell <apurcell88@gmail.com>
2025-08-14 15:58:31 -0500

    Debugging another route.

--- src/app/api/applications/[id]/route.ts
+++ src/app/api/applications/[id]/route.ts
@@ -1,13 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PrismaClient } from "@/generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
+export async function PUT(req: NextRequest, context: any) {
+  const id = context.params.id;
 
   try {
     const data = await req.json();
