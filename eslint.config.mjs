adfa83622f3415b248d6fe0f4566f7939f0cf285
Adam Purcell <apurcell88@gmail.com>
2025-08-15 17:08:47 -0500

    More debugging.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,14 @@
-// eslint.config.js
-import { defineConfig } from "eslint-define-config";
-import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname });
 
-export default defineConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-  {
-    ignores: ["src/generated/**"], // <- important: ignore Prisma-generated code
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-      },
-    },
-    rules: {
-      "@typescript-eslint/no-require-imports": "error",
-    },
-  },
-]);
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
